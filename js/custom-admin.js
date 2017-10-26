60c68d5c4ff927f954eebb290d6f0039e94bd3bd
Tu Nguyen <nvminhtu@gmail.com>
2017-10-26 15:20:48 +0700

    add custom javascript - check text and notification

--- js/custom-admin.js
+++ js/custom-admin.js
@@ -1,10 +1,20 @@
 jQuery(function($) {
 	
-	var $introduction = $('#field_introduction textarea'),
+	var 
+		$introduction = $('#field_introduction textarea'),
 		$messNotify = $('#notify_intro .acf-input'),
+		$changeNotify = $('#change_notification_text .acf-input input[type="text"]');
+
+	var maxlength = $('#field_introduction textarea').attr('maxlength');
 		
+	var notifyChars = $changeNotify.val(),
+		chars = notifyChars.split('xx');
+
 		curLength = $introduction.val().length,
-		leftChars = 150 - curLength;
+		leftChars = maxlength - curLength;
+
+	
+		leftChars = chars[0] + leftChars + chars[1];
 
 	$messNotify.text(leftChars);
 
@@ -12,10 +22,12 @@ jQuery(function($) {
 	$introduction.bind('input', function() {
 	  var 
 	  	selected_length = $(this).val().length;
-	  	left_length = 150 - selected_length;
+	  	left_length = maxlength - selected_length;
 
-	  if (selected_length <= 150) {
-		$messNotify.text(left_length + ' characters remaining.');
+	  left_length = chars[0] + left_length + chars[1];
+
+	  if (selected_length <= maxlength) {
+		$messNotify.text(left_length);
 	  }
 	})
 });
